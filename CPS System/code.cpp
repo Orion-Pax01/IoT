2d6a91c4fc719745090a1dcdf1fef6185dc8c296
Orion-Pax01 <114070333+Orion-Pax01@users.noreply.github.com>
2024-10-05 23:27:50 +0530

    Create code.cpp

---
+++ CPS System/code.cpp
@@ -0,0 +1,29 @@
+
+int ledPin = 13;  // Built-in LED on Arduino Uno
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);   // Set the LED pin as an output
+  Serial.begin(9600);        // Start serial communication at 9600 baud rate
+}
+
+void loop() 
+{
+  if (Serial.available() > 0) 
+  {      
+    // Check if data is available on serial port
+    char command = Serial.read();    // Read the command sent over serial
+    if (command == '1') 
+    {
+      // If command is '1', turn on LED
+      digitalWrite(ledPin, HIGH);
+      Serial.println("LED ON");
+    } 
+    else if (command == '0') 
+    {     
+      // If command is '0', turn off LED
+      digitalWrite(ledPin, LOW);
+      Serial.println("LED OFF");
+    }
+  }
+}
